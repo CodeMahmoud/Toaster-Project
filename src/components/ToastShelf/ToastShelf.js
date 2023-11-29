31b4def7543bdd5e05eb72b4543c1057eaeb312b
CodeMahmoud <mahmoudcode913@gmail.com>
2023-11-29 15:48:55 -0600

    made changes to improve screan reader when reading toast notifications

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -7,7 +7,11 @@ import { ToastContext } from '../ToastProvider/ToastProvider';
 function ToastShelf() {
   const { toasts } = React.useContext(ToastContext);
   return (
-    <ol className={styles.wrapper}>
+    <ol className={styles.wrapper}
+    role='region'
+    aria-live='polite'
+    aria-label='Notifications'
+    >
       {toasts.map((toast) => {
         return (
       <li key={toast.id} className={styles.toastWrapper}>
